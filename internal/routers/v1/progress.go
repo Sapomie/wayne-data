dc7d5526d705dcb659037d7049638f2a7a9bbc1c
Sapomie <vavavvvaaa@126.com>
2021-11-12 05:53:00 +0800

    页面相对路径修改.

--- internal/routers/v1/progress.go
+++ internal/routers/v1/progress.go
@@ -11,28 +11,10 @@ import (
 	"time"
 )
 
-func GetProgressTenNow(c *gin.Context) {
-	response := app.NewResponse(c)
-	zone := mtime.NewMTime(cons.Newest.Add(-1 * time.Hour)).TimeZone(mtime.TypeTen)
-	startTime := time.Date(2021, 1, 1, 0, 0, 0, 0, time.Local)
-
-	svc := progress.NewEssentialService(c)
-	pro, err := svc.GetProgress(zone, startTime)
-	if err != nil {
-		global.Logger.Errorf(c, "svc.GetProgress err: %v", err)
-		response.ToErrorResponse(errcode.ErrorGetProgressFail)
-		return
-	}
-	response.ToResponseHtml("progress.html", gin.H{
-		"progress": pro,
-	})
-}
-
 func GetProgressMonthNow(c *gin.Context) {
 	response := app.NewResponse(c)
 
 	typ := mtime.NewTimeTypeByStr(c.Param("typ"))
-
 	zone := mtime.NewMTime(cons.Newest.Add(-1 * time.Hour)).TimeZone(typ)
 	startTime := time.Date(2021, 1, 1, 0, 0, 0, 0, time.Local)
 
